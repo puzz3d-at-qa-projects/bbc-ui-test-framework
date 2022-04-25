8a7cf4d2578c8de45706a95bf717289d3c4aa1f9
Dmytro Hryshchenko <puzz3d@gmail.com>
2022-04-25 23:04:49 +0300

    steps optimised.

--- src/test/java/stepdefinitions/FormSteps.java
+++ src/test/java/stepdefinitions/FormSteps.java
@@ -1,11 +1,11 @@
 package stepdefinitions;
 
 import actionclasses.FormQueries;
+import complexelements.Form;
 import io.cucumber.java.en.And;
 import manager.PageFactoryManager;
 import org.jetbrains.annotations.NotNull;
 import org.openqa.selenium.WebDriver;
-import sharedcontext.ScenarioContext;
 import sharedcontext.TestContext;
 
 import java.time.Duration;
@@ -19,32 +19,40 @@ public class FormSteps {
     WebDriver driver;
     PageFactoryManager pageFactoryManager;
     FormQueries form;
-    ScenarioContext scenarioContext;
+    Form formToMeetTaskRequirements;
 
     public FormSteps(@NotNull TestContext testContext) {
         driver = testContext.getDriver();
         pageFactoryManager = testContext.getPageFactoryManager();
-//        navigate = testContext.getNavigate();
-        scenarioContext = testContext.scenarioContext;
         form = new FormQueries(pageFactoryManager);
+        formToMeetTaskRequirements = new Form(pageFactoryManager);
     }
 
-    @And("User trying to submit the form with incorrect entered data")
+    @And("the user trying to submit the form with incorrectly entered data")
     public void userTryingToSubmitTheFormWithIncorrectEnteredData(List<Map<String,String>> formData) {
-        form.fillForm(formData);
-        form.acceptTermsOfService();
-        form.submit();
+        formToMeetTaskRequirements.fillForm(formData);
     }
 
-    @And("Error message {string} displayed")
-    public void errorMessageCanTBeBlankDisplayed(String errorMessage) {
+    @And("the error message {string} displayed")
+    public void errorMessageCanTBeBlankDisplayed(String expectedErrorMessage) {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//        System.out.println("Error msg: " + errorMessage);
-//        System.out.println("Error before clicks? " + form.errorDisplayed(errorMessage));
         form.clickTermsOfService(); // had to add these clicks to clear error from previous iteration
         form.clickTermsOfService(); // it was "cached" somehow, couldn't fix it with any waits but Thread.sleep, which is forbidden))
         form.clickTermsOfService(); // with just one click test was unstable, with three works fine ☺
-//        System.out.println("Error after clicks? " + form.errorDisplayed(errorMessage));
-        assertTrue(form.errorDisplayed(errorMessage));
+        System.out.println("fem:"+form.errorMessage());
+        System.out.println("eem:"+expectedErrorMessage);
+        assertTrue(form.errorMessage().contains(expectedErrorMessage));
+    }
+
+    @And("the user trying to submit the form with incorrectly entered dataData")
+    public void theUserTryingToSubmitTheFormWithIncorrectlyEnteredData(Map<String,String> formData) {
+        formToMeetTaskRequirements.fillForm(formData);
+    }
+
+    @And("the error message displayed")
+    public void theErrorErrormsgDisplayed(@NotNull Map<String, String> expectedErrorMessage) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String key = expectedErrorMessage.keySet().iterator().next();
+        assertTrue(form.errorMessage().contains(expectedErrorMessage.get(key)));
     }
 }
