1b01d9a82a9bf09e484dd9e6e7d9f7e1ac147b03
Dmytro Hryshchenko <puzz3d@gmail.com>
2022-04-25 23:05:19 +0300

    new steps file.

---
+++ src/test/java/stepdefinitions/SportScoresSteps.java
@@ -0,0 +1,63 @@
+package stepdefinitions;
+
+import actionclasses.SportScoresQueries;
+import complexelements.ScoreBoard;
+import io.cucumber.java.en.And;
+import manager.PageFactoryManager;
+import org.jetbrains.annotations.NotNull;
+import org.openqa.selenium.WebDriver;
+import sharedcontext.TestContext;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class SportScoresSteps {
+
+    WebDriver driver;
+    PageFactoryManager pageFactoryManager;
+    SportScoresQueries scores;
+    ScoreBoard scoreBoard;
+
+    public SportScoresSteps(@NotNull TestContext testContext) {
+        driver = testContext.getDriver();
+        pageFactoryManager = testContext.getPageFactoryManager();
+        scores = new SportScoresQueries(pageFactoryManager);
+        scoreBoard = new ScoreBoard(pageFactoryManager);
+    }
+
+    @And("the user made a search by {string}")
+    public void userMadeASearchByChampionshipName(String league) {
+        scores.searchLeague(league);
+        scores.clickOnLeagueInDropdownNamed(league);
+    }
+
+    @And("the user select a {string}")
+    public void userSelectAMonth(String month) {
+        scores.clickOnMonth(month);
+    }
+
+    @And("there is a match between {string} and {string} with {string} - {string} on the Scores & Fixtures page")
+    public void thereIsAMatchOnTheScoresFixturesPage(String team1, String team2, String team1Score, String team2Score) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        assertTrue(scores.matchIsOnTheScoresAndFixturesPage(team1, team2, team1Score, team2Score));
+    }
+
+    @And("the user clicks on 'Home team'")
+    public void userClicksOnTeam1() {
+        scores.clickOnTeam1();
+    }
+
+    @And("there is a match between {string} and {string} with {string} - {string} on the Match Overview page")
+    public void thereIsAMatchOnTheOverviewPage(String team1, String team2, String team1Score, String team2Score) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        assertTrue(scores.matchIsOnTheMatchOverviewPage(team1, team2, team1Score, team2Score));
+    }
+
+    @And("the score of the match between {string} and {string} is {string} - {string}")
+    public void theScoreOfTheMatchBetweenTeamAndTeamIs(String team1, String team2, String team1Score, String team2Score) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        assertEquals(team1Score + " - " + team2Score, scoreBoard.getScore(team1, team2).toString());
+    }
+}
