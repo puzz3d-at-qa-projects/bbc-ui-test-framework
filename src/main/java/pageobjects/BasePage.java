4df62dfff3beebe5c8eac47443c27fa97089c5c5
Dmytro Hryshchenko <puzz3d@gmail.com>
2022-09-13 00:36:27 +0300

    AjaxElementLocatorFactory added.

--- src/main/java/pageobjects/BasePage.java
+++ src/main/java/pageobjects/BasePage.java
@@ -3,6 +3,7 @@ package pageobjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -14,7 +15,7 @@ public class BasePage {
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver, this);
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 10), this);
     }
 
     public void waitVisibilityOfElement(long timeToWait, WebElement element) {
